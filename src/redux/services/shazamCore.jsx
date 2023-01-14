da6172a5c570efab950485c08af672d6bf901c39
Donkor T. Jonas <59501716+kwqme-Jonas@users.noreply.github.com>
2023-01-14 00:15:17 +0000

    updated codebase

--- src/redux/services/shazamCore.jsx
+++ src/redux/services/shazamCore.jsx
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const shazamCoreApi = createApi({
   reducerPath: 'shazamCoreApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    baseUrl: 'https://shazam-core.p.rapidapi.com/',
     prepareHeaders: (headers) => {
       headers.set('X-RapidAPI-Key', '5067b9a664msh690e26811843a6ap1de178jsne3a9491cd2dd');
 
