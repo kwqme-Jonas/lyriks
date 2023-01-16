50dffd73eb1768cc70410ad523efca26b9b104d2
Donkor T. Jonas <59501716+kwqme-Jonas@users.noreply.github.com>
2023-01-16 19:35:56 +0000

    made some updates

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -11,9 +11,22 @@ import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 import 'swiper/css';
 import 'swiper/css/free-mode';
 
-const TopChartCard = () => (
+const TopChartCard = ({ song, i }) => (
   <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-ponter">
-    {song.title}
+    <h3 className="font-bold text-base text-white mr-3">
+      {i + 1}.
+    </h3>
+    <div className="flex-1 flex flex-row justify-between items-center">
+      <img className="w-20 h-20 rounded-lg" src={song?.images?.coverart} alt={song?.title} />
+      <div className="flex-1 flex flex-col justify-center mx-3">
+        <Link to={`/song/${song.key}`}>
+          <p className="text-xl font-bold text-white">{song?.title}</p>
+        </Link>
+        <Link to={`/artist/${song?.artist[0].adamid}`}>
+          <p className="text-base font-bold text-gray-300 mt-1">{song?.subtitle}</p>
+        </Link>
+      </div>
+    </div>
   </div>
 );
 
@@ -58,6 +71,41 @@ const TopPlay = () => {
             />
           ))}
         </div>
+      </div>
+      <div className="w-full flex flex-col mt-8">
+        <div className="flex flex-row justify-between items-center">
+          <h2 className="text-white font-bold text-2xl">
+            Top artists
+          </h2>
+          <Link to="/top-artist">
+            <p className="text-gray-300 text-base cursor-pointer">See More</p>
+          </Link>
+        </div>
+        <Swiper
+          slidesPerView="auto"
+          spaceBetween={15}
+          freeMode
+          centeredSlides
+          centeredSlidesBounds
+          modules={[FreeMode]}
+          className="mt-4"
+        >
+          {topPlays?.map((song, i) => (
+            <SwiperSlide
+              key={song?.key}
+              style={{ width: '25%', height: 'auto' }}
+              className="shadow-lg rounded-full animate-slideright"
+            >
+              <Link to={`/artists/${song?.artists[0].adamid}`}>
+                <img
+                  src={song?.images.background}
+                  alt="name"
+                  className="rounded-full w-full object-cover"
+                />
+              </Link>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
