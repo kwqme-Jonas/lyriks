ac8c061bf1e81bd1b8ff58e0efc1b1e85cbbf9f4
Donkor T. Jonas <59501716+kwqme-Jonas@users.noreply.github.com>
2023-01-20 21:49:15 +0000

    updated codebase

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -3,11 +3,13 @@ import { useSelector, useDispatch } from 'react-redux';
 import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
 
 import { setActiveSong, playPause } from '../redux/features/playerSlice';
+import { useGetSongDetailsQuery } from '../redux/services/shazamCore';
 
 const SongDetails = () => {
   const dispatch = useDispatch();
   const { songid } = useParams();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data: songData, is isFetching: isFetchingSongDetails } = useGetSongDetailsQuery({ songid });
 
   console.log(songid);
 
@@ -21,6 +23,13 @@ const SongDetails = () => {
         <h2 className="text-white text-3xl font-bold">
           Lyrics:
         </h2>
+
+        <div className='mt-5'>
+          {songData?.sections[1].type === 'LYRICS'
+            ? songData?.sections[1].text.map((line, i) => (
+              <p>{line}</p>
+          )): <p>Sorry, no lyrics found</p>}
+        </div>
       </div>
     </div>
   );
