facc3ffeac85d2fd81c6fd59e34b854703e8d55f
Donkor T. Jonas <59501716+kwqme-Jonas@users.noreply.github.com>
2023-01-20 22:26:52 +0000

    updated codebase

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -9,26 +9,26 @@ const SongDetails = () => {
   const dispatch = useDispatch();
   const { songid } = useParams();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
-  const { data: songData, is isFetching: isFetchingSongDetails } = useGetSongDetailsQuery({ songid });
+  const { data: songData, isFetching: isFetchingSongDetails } = useGetSongDetailsQuery({ songid });
 
   console.log(songid);
 
   return (
     <div className="flex flex-col">
-      {/* <DetailsHeader
-              artistId={artistId}
-              songData={songData}
-  /> */}
+      <DetailsHeader
+        artistId=""
+        songData={songData}
+      />
       <div className="mb-10">
         <h2 className="text-white text-3xl font-bold">
           Lyrics:
         </h2>
 
-        <div className='mt-5'>
+        <div className="mt-5">
           {songData?.sections[1].type === 'LYRICS'
             ? songData?.sections[1].text.map((line, i) => (
-              <p>{line}</p>
-          )): <p>Sorry, no lyrics found</p>}
+              <p className="text-gray-400 text-base my-1">{line}</p>
+            )) : <p className="text-gray-400 text-base my-1">Sorry, no lyrics found</p>}
         </div>
       </div>
     </div>
