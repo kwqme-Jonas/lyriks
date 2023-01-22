529d2e15e0e4b0e2d9092f6d6e22abb409a8cf21
Donkor T. Jonas <59501716+kwqme-Jonas@users.noreply.github.com>
2023-01-22 15:34:34 +0000

    updated AroundYou.jsx

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -4,12 +4,13 @@ import { useSelector } from 'react-redux';
 
 import { Error, Loader, SongCard } from '../components';
 
+import { useGetSongsByCountryQuery } from '../redux/services/shazamCore';
+
 const AroundYou = () => {
   const [country, setCountry] = useState('');
   const [loading, setLoading] = useState(true);
   const { activeSong, isPlaying } = useSelector((state) => state.player);
-
-  console.log(country);
+  const { data, isFetching, error } = useGetSongsByCountryQuery(country);
 
   useEffect(() => {
     axios.get('https://geo.ipify.org/api/v1?apiKey=at_nkhsjdCt6mf1Noge8Whmpa7tVQrSx')
@@ -17,6 +18,10 @@ const AroundYou = () => {
       .catch((err) => console.log(err))
       .finally(() => setLoading((false)));
   }, [country]);
+
+  if (isFetching && loading) return <Loader title="Loading songs around you" />;
+
+  if (error && country) return <Error />;
 
   return (
     <div />
