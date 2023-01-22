9435650a42a711fd960cbec25b8f516c10411f58
Donkor T. Jonas <59501716+kwqme-Jonas@users.noreply.github.com>
2023-01-22 15:47:12 +0000

    finished AroundYou.jsx

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -24,7 +24,23 @@ const AroundYou = () => {
   if (error && country) return <Error />;
 
   return (
-    <div />
+    <div className="flex flex-col">
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+        Around You <span className="font-black">{country}</span>
+      </h2>
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+            i={i}
+          />
+        ))}
+      </div>
+    </div>
   );
 };
 export default AroundYou;
