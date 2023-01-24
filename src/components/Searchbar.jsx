c645181290fd61251104ff2fc1b8dd27347388f4
Donkor T. Jonas <59501716+kwqme-Jonas@users.noreply.github.com>
2023-01-24 15:44:06 +0000

    mad esome changes

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import {FiSearch} from 'react-icons/fi';
+import { FiSearch } from 'react-icons/fi';
 
 const Searchbar = () => (
   <form autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600">
